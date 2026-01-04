34de387fc9398196097307902bf82d7e48e93161
Arvitum <142514521+Arvitum@users.noreply.github.com>
2026-01-04 20:13:10 +0200

    Update ArvitumAppTestApplication.java

--- src/main/java/com/example/arvitumapptest/ArvitumAppTestApplication.java
+++ src/main/java/com/example/arvitumapptest/ArvitumAppTestApplication.java
@@ -3,11 +3,13 @@ package com.example.arvitumapptest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.ai.tool.annotation.ToolParam;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class ArvitumAppTestApplication {
 
     public static void main(String[] args) {
+        List<String> stringlist = new ArrayList<>();
         SpringApplication.run(ArvitumAppTestApplication.class, args);
     }
 
